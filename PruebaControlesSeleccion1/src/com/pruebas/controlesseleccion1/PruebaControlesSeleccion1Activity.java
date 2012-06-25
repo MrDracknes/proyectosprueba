f8b67da37137cb5bdd2f925f94f2e4ff0eaf18dc
MrDracknes <jesus.dracknes@gmail.com>
2012-06-25 17:07:42 +0200

    Añadido la prueba de controles de seleccion 1. Finalizado.

--- PruebaControlesSeleccion1/src/com/pruebas/controlesseleccion1/PruebaControlesSeleccion1Activity.java
+++ PruebaControlesSeleccion1/src/com/pruebas/controlesseleccion1/PruebaControlesSeleccion1Activity.java
@@ -2,12 +2,45 @@ package com.pruebas.controlesseleccion1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class PruebaControlesSeleccion1Activity extends Activity {
     /** Called when the activity is first created. */
+	
+	
+	Spinner lista;
+	
+	TextView lblMensaje;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+    	final String[] datos =
+    	        new String[]{"Elem1","Elem2","Elem3","Elem4","Elem5"};
+    	
+    	ArrayAdapter<String> adaptador =
+    	        new ArrayAdapter<String>(this,
+    	            android.R.layout.simple_spinner_item, datos);
+        lista = (Spinner)findViewById(R.id.CmbOpciones);
+        adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        lista.setAdapter(adaptador);
+        lblMensaje = (TextView)findViewById(R.id.tv1);
+        lista.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                public void onItemSelected(AdapterView<?> parent,
+                    android.view.View v, int position, long id) {
+                        lblMensaje.setText("Seleccionado: " + datos[position]);
+                }
+         
+                public void onNothingSelected(AdapterView<?> parent) {
+                    lblMensaje.setText("");
+                }
+        }); 
+        
     }
 }
